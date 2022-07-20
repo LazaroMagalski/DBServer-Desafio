562ac5cdd2b03a4099066db1f1fc25d3064d14e3
kazaku <lazaro.magalski@ggmail.com>
2022-07-20 18:21:26 -0300

    adicionando arquivos

--- desafio/desafio.js
+++ desafio/desafio.js
@@ -1,12 +1,11 @@
 const readline = require('readline-sync');
 const Forca = require('./forca');
 
-const jogo = new Forca('abacaxi');
-
+const jogo = new Forca('jaca');
 while (!["perdeu", "ganhou"].includes(jogo.buscarEstado())) {
     const chute = readline.question("Aguardando chute: \n");
     jogo.chutar(chute);
-    console.log(jogo.buscarDadosDoJogo());
+    console.log(jogo.buscarDadosDoJogo());  
 }
 
 console.log("você " + jogo.buscarEstado());
--- desafio/forca.js
+++ desafio/forca.js
@@ -1,14 +1,51 @@
 class Forca {
+  constructor(nome){
+    this.nome = nome;
+    this.palavra = Array(nome.length).fill('_');
+    this.letrasChutadas = [];
+    this.vidas = 6;
+  }
+  
+  chutar(letra) {
+    let acertou = false;
+    if(letra.length == 1){
+      //Verifica se a letra é repetida
+      for(let i =0; i< this.letrasChutadas.length; i++){
+        if(this.letrasChutadas[i] == letra){
+          return;
+        }
+      } 
+      //preenche o array com a letra
+      for(let i = 0; i < this.nome.length; i++){
+          if(this.nome[i] == letra){
+            this.palavra[i] = this.nome[i];
+            acertou = true;
+          }
+        }
+      //Se a letra não foir escolhida diminui a vida 
+      if(acertou == false){
+        this.vidas--;
+      }
+      console.log("letra chutada:"+ letra);
+      this.letrasChutadas.push(letra);
+    }
+  }
 
-  chutar(letra) { }
-
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  buscarEstado() { 
+    if(this.nome == this.palavra.toString().replaceAll(",", "")){
+      return "ganhou";
+    }else if(this.vidas == 0){
+      return "perdeu";
+    }else if(this.vidas > 0){
+      return "aguardando chute";
+    }
+    return "saiu"; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
       return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+          letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
+          vidas: this.vidas, // Quantidade de vidas restantes
+          palavra: this.palavra // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
       }
   }
 }
